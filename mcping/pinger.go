b6d84a42ea8c7f3998f0e96488d3f82284062a4d
qugalet <qugalet@m0e.space>
2024-02-04 16:30:03 +0200

    make pinger struct public

--- mcping/pinger.go
+++ mcping/pinger.go
@@ -11,40 +11,40 @@ import (
 	"github.com/iverly/go-mcping/latency"
 )
 
-type pinger struct {
+type Pinger struct {
 	DnsResolver types.DnsResolver
 	Latency     types.Latency
 }
 
 // Create a new Minecraft Pinger
-func NewPinger() *pinger {
+func NewPinger() *Pinger {
 	var resolver types.DnsResolver
 	resolver = dns.NewResolver()
-	return &pinger{DnsResolver: resolver}
+	return &Pinger{DnsResolver: resolver}
 }
 
 // Create a new Minecraft Pinger with a custom DNS resolver
-func NewPingerWithDnsResolver(dnsResolver types.DnsResolver) *pinger {
-	return &pinger{DnsResolver: dnsResolver}
+func NewPingerWithDnsResolver(dnsResolver types.DnsResolver) *Pinger {
+	return &Pinger{DnsResolver: dnsResolver}
 }
 
 // Ping and get information from an host and port, default timeout: 3s
 // Return a pointer to types.PingResponse or an error
 //
-// Error is thrown when the host is unreachable or the data received are incorrect
+// # Error is thrown when the host is unreachable or the data received are incorrect
 //
-// Example: pinger.Ping("play.hypixel.net", 25565)
-func (p *pinger) Ping(host string, port uint16) (*types.PingResponse, error) {
+// Example: Pinger.Ping("play.hypixel.net", 25565)
+func (p *Pinger) Ping(host string, port uint16) (*types.PingResponse, error) {
 	return p.PingWithTimeout(host, port, 3*time.Second)
 }
 
 // Ping and get information from an host and port with a custom timeout
 // Return a pointer to types.PingResponse or an error
 //
-// Error is thrown when the host is unreachable or the data received are incorrect
+// # Error is thrown when the host is unreachable or the data received are incorrect
 //
 // Example: pinger.Ping("play.hypixel.net", 25565, 5 * time.Second)
-func (p *pinger) PingWithTimeout(host string, port uint16, timeout time.Duration) (*types.PingResponse, error) {
+func (p *Pinger) PingWithTimeout(host string, port uint16, timeout time.Duration) (*types.PingResponse, error) {
 	resolve, hostSRV, portSRV := p.DnsResolver.SRVResolve(host)
 	if resolve {
 		host = hostSRV
